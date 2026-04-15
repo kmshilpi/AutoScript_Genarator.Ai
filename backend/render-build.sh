6d91aed381c85e558d23a0f0e140daa9d26ffc44
kmshilpi <kmshilpi@wattmonk.com>
2026-04-15 22:38:09 +0530

    fix(deploy): return to project root before pip install

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+# Store the root path of the project
+PROJECT_ROOT=$(pwd)
 STORAGE_DIR=/opt/render/project/.render
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
@@ -15,6 +17,9 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
 else
   echo "...Using Chrome from cache"
 fi
+
+# Return to project root before installing requirements
+cd $PROJECT_ROOT
 
 # Install python dependencies as usual
 pip install -r requirements.txt
